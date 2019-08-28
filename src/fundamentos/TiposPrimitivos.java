ede8f5fd381b64892097a567926a5ee91f65ae93
tampieri <higortampieri@gmail.com>
2019-08-28 09:49:54 -0300

    Fundamentos Java tipos Primitivos na pratica

--- src/fundamentos/TiposPrimitivos.java
+++ src/fundamentos/TiposPrimitivos.java
@@ -3,7 +3,29 @@ package fundamentos;
 public class TiposPrimitivos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Tipos Primitivos
+		byte anosIdade = 39;
+		short numeroShort = 542;
+		int id = 56932;
+		long idLong = 3_123_456_333L;
+		
+		float salario = 11_445.44F;
+		double vendas = 2_991_898_111.01;
+		
+		boolean estado = false;
+		
+		char status = '1';
+		
+		System.out.println(anosIdade);
+		System.out.println(numeroShort);
+		System.out.println(id);
+		System.out.println(idLong);
+		System.out.println(salario);
+		System.out.println(vendas);
+		System.out.println(estado);
+		System.out.println(status);
+		
+		
 
 	}
 
