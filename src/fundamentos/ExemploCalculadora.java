ad1b42b359d01614fc83bc42304ad0ae109ba7e4
tampieri <higortampieri@gmail.com>
2019-09-01 11:40:23 -0300

    ExemploCalculadora

---
+++ src/fundamentos/ExemploCalculadora.java
@@ -0,0 +1,31 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class ExemploCalculadora {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Informe o numero: ");
+		double num1 = entrada.nextDouble();
+		
+		System.out.print("Informe o numero: ");
+		double num2 = entrada.nextDouble();
+		
+		System.out.print("Informe o operador: ");
+		String operador = entrada.next();
+		
+		//Logica da operação
+		double result = "+".equals(operador) ? num1 + num2 : 0;
+		result = "-".equals(operador) ? num1 - num2 : result;
+		result = "*".equals(operador) ? num1 * num2 : result;
+		result = "/".equals(operador) ? num1 / num2 : result;
+		result = "%".equals(operador) ? num1 % num2 : result;;
+		
+		System.out.printf("%.2f %s %.2f = %.2f", num1, operador, num2, result);
+		
+		entrada.close();
+	}
+}
