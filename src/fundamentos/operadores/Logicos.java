b8885da380dddb05bd1ace31a47ef383afce5d75
tampieri <higortampieri@gmail.com>
2019-08-31 22:01:22 -0300

    Logicos

---
+++ src/fundamentos/operadores/Logicos.java
@@ -0,0 +1,40 @@
+package fundamentos.operadores;
+
+public class Logicos {
+
+	public static void main(String[] args) {
+		
+		boolean condicao1 = true;
+		boolean condicao2 = 3 > 7;
+		
+		System.out.println(condicao1 && condicao2);
+		System.out.println(condicao1 && !condicao2);
+		System.out.println(condicao1 || condicao2);
+		System.out.println(condicao1 ^ condicao2);
+		System.out.println(!!condicao1);
+		System.out.println(!condicao1);
+		
+		//tabela verdade
+		System.out.println("AND");
+		System.out.println(true && true);
+		System.out.println(true && false);
+		//System.out.println(false && true);
+		//System.out.println(false && false);
+		
+		System.out.println("OR");
+		//System.out.println(true || true);
+		//System.out.println(true || false);
+		System.out.println(false || true);
+		System.out.println(false || false);
+		
+		System.out.println("OR exclusivo");
+		System.out.println(true ^ true);
+		System.out.println(true ^ false);
+		System.out.println(false ^ true);
+		System.out.println(false ^ false);
+		
+		System.out.println("NOT");
+		System.out.println(!true);
+		System.out.println(!false);
+	}
+}
