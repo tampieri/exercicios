95f5c3dd759860dde34aee14a43fcbcdd68ad40d
tampieri <higortampieri@gmail.com>
2019-09-02 10:28:00 -0300

    Conceito de construtor

--- src/classe/Produto.java
+++ src/classe/Produto.java
@@ -6,4 +6,29 @@ public class Produto {
 	double preco;
 	double desconto;
 	
+	public Produto() {
+	}
+	
+	Produto(String nomeInicial){
+		nome = nomeInicial;
+	}
+	
+	Produto(String nomeInicial, double precoInicial){
+		nome = nomeInicial;
+		preco = precoInicial;
+	}
+	
+	Produto(String nomeInicial, double precoInicial, double descontoInicial){
+		nome = nomeInicial;
+		preco = precoInicial;
+		desconto = descontoInicial;
+	}
+	
+	double precoComDesconto() {
+		return preco * (1 - desconto);
+	}
+	
+	public double precoComDesconto(double descontoAuxiliar) {
+		return preco * (1 - desconto + descontoAuxiliar);
+	}	
 }
--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -4,22 +4,30 @@ public class ProdutoTeste {
 
 	public static void main(String[] args) {
 
-		Produto p1 = new Produto();
-		p1.nome = "notebook";
-		p1.preco = 1000.00;
-		p1.desconto = 0.25;
+		var p1 = new Produto();
+		p1.nome = "Lapis";
+		p1.preco = 1.00;
+		p1.desconto = 0.30;
 		
-		var p2 = new Produto();
-		p2.nome = "Lapis";
-		p2.preco = 1.00;
-		p2.desconto = 0.30;
+		Produto p2 = new Produto("notebook");
+		p2.preco = 1000.00;
+		p2.desconto = 0.25;
+				
+		Produto p3 = new Produto("penDrive", 25.65);
+		p3.desconto = 0.25;
+		
+		Produto p4 = new Produto("Impressora", 250.65, 0.20);
 		
 		System.out.println(p1.nome);
 		System.out.println(p2.nome);
+		System.out.println(p3.nome);
+		System.out.println(p4.nome);
 		
-		double precoFinal1 = p1.preco *(1 - p1.desconto);
-		double precoFinal2 = p2.preco *(1 - p2.desconto);
-		double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
+		double precoFinal1 = p1.precoComDesconto();
+		double precoFinal2 = p2.precoComDesconto();
+		double precoFinal3 = p3.precoComDesconto(0.1);
+		double precoFinal4 = p4.precoComDesconto(0.11);
+		double mediaCarrinho = (precoFinal1 + precoFinal2 + precoFinal3 + precoFinal4) / 4;
 		System.out.printf("Média do carrinho = R$%.2f.", mediaCarrinho);
 	}
 
