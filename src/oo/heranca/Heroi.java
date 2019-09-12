6205992897a11108f84fa5958e2ee0b7ee607d47
tampieri <higortampieri@gmail.com>
2019-09-12 08:41:13 -0300

    Conceito Orientação a Objeto - Conceito de Herança parte2

--- src/oo/heranca/Heroi.java
+++ src/oo/heranca/Heroi.java
@@ -2,6 +2,10 @@ package oo.heranca;
 
 public class Heroi extends Jogador {
 	
+	Heroi(int x, int y){
+		super(x, y);
+	}
+	
 	boolean atacar(Jogador oponente) {
 		
 		/*
--- src/oo/heranca/jogo.java
+++ src/oo/heranca/jogo.java
@@ -3,13 +3,9 @@ package oo.heranca;
 public class Jogo {
 	
 	public static void main(String[] args) {
-		Heroi heroi = new Heroi();
-		heroi.x = 10;
-		heroi.y = 10;
+		Heroi heroi = new Heroi(10, 10);
 		
 		Monstro monstro = new Monstro();
-		monstro.x = 10;
-		monstro.y = 11;
 		
 		System.out.println("Heroi tem  = " + heroi.vida + " vida");
 		System.out.println("Monstro tem  = " + monstro.vida + " vida");
