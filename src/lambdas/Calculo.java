40efb721b78594d2f759b5ba591d356f3b630709
tampieri <higortampieri@gmail.com>
2019-09-12 22:33:26 -0300

    Conceito Orientação a Objeto - Conceito de Lambda - interface funcional

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,6 +1,18 @@
 package lambdas;
 
+@FunctionalInterface
 public interface Calculo {
 
-	public abstract double executar(double a, double b);
+	double executar(double a, double b);
+	//uma interface funcional so pode ter um metodo
+	//double teste(int a);
+	
+	//pode existir outros metodos mas somente um abstrato
+	default String MetodoNaoAbstrato1() {
+		return "Texto qualquer";
+	}
+	
+	static String MetodoNaoAbstrato2() {
+		return "Outro texto qualquer";
+	}
 }
--- src/lambdas/CalculoTeste.java
+++ src/lambdas/CalculoTeste.java
@@ -9,6 +9,8 @@ public class CalculoTeste {
 		
 		calculo = new Multiplicar();
 		System.out.println(calculo.executar(2, 4));
+		
+		System.out.println(calculo.MetodoNaoAbstrato1());
+		System.out.println(Calculo.MetodoNaoAbstrato2());
 	}
-
 }
