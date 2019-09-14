ba7d5e45ecfe700ae50abcc8b0dc54ec7bb230e5
tampieri <higortampieri@gmail.com>
2019-09-13 21:51:29 -0300

    Conceito Orientação a Objeto - Conceito de Lambda - Predicate 2

---
+++ src/lambdas/PredicadoComposicao.java
@@ -0,0 +1,18 @@
+package lambdas;
+
+import java.util.function.Predicate;
+
+public class PredicadoComposicao {
+
+	public static void main(String[] args) {
+		
+		Predicate<Integer> isPar = num -> num % 2 == 0;
+		Predicate<Integer> isTresDigitos = num -> num >= 100 && num <= 999;
+		
+		System.out.println(isPar.and(isTresDigitos).test(322));
+		System.out.println(isPar.and(isTresDigitos).test(321));
+		System.out.println(isPar.and(isTresDigitos).negate().test(322));
+		System.out.println(isPar.or(isTresDigitos).test(32));
+	}
+
+}
