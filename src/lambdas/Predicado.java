a2a6ed4957db141f6ce683cb1e99b4d1bffd45f2
tampieri <higortampieri@gmail.com>
2019-09-13 21:13:48 -0300

    Conceito Orientação a Objeto - Conceito de Lambda - BinaryOperator e Predicate

---
+++ src/lambdas/Predicado.java
@@ -0,0 +1,15 @@
+package lambdas;
+
+import java.util.function.Predicate;
+
+public class Predicado {
+
+	public static void main(String[] args) {
+		
+		Predicate<Produto> isCaro = 
+				prod -> (prod.preco * (1 - prod.desconto)) >= 750;
+		
+		Produto produto = new Produto("computador", 1500.00, 0.10);
+		System.out.println(isCaro.test(produto));
+	}
+}
