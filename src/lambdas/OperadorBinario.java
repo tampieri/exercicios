19de952a99f52dfdcf430c79143d928645a22e62
tampieri <higortampieri@gmail.com>
2019-09-13 22:34:10 -0300

    Conceito Orientação a Objeto - Conceito de Lambda - BinaryOperator e BiFunction

---
+++ src/lambdas/OperadorBinario.java
@@ -0,0 +1,29 @@
+package lambdas;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class OperadorBinario {
+
+	public static void main(String[] args) {
+		
+		BinaryOperator<Double> media = (n1, n2) -> (n1 + n2) / 2;
+		System.out.println(media.apply(8.9, 5.8));
+		
+		BiFunction<Double, Double, String> resultado = (n1, n2) -> {
+			double notaFinal = (n1 + n2) / 2;
+			return notaFinal >= 7 ? "Aprovado" : "Reprovado";
+		};
+		
+		System.out.println(resultado.apply(8.0, 4.0));
+		System.out.println(resultado.apply(8.0, 7.0));
+		
+		Function<Double, String> conceito = m -> m >= 7 ? "Aprovado" : "Reprovado";
+		
+		System.out.println(media.andThen(conceito).apply(9.9, 5.1));
+		System.out.println(media.andThen(conceito).apply(5.9, 5.1));
+
+	}
+
+}
